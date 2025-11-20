b2dfcb3e5afcd4a591898488afffb9e73d17f73c
Raven <2568333411@qq.com>
2025-11-20 18:33:43 +0800

    day03 60

---
+++ 19_单文件组件/School.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="demo">
+    <h2>{{name}}</h2>
+    <button @click='out'>shuchu</button>
+  </div>
+        
+    
+</template>
+<script>
+export default {
+      name:"School",
+      data() {
+        return { name: 'xzy' }
+      },
+      methods: {
+        out(){
+          console.log(this.name);
+        }
+      },
+}
+
+</script>
+<style>
+  .demo{
+    background-color: #b83d3d;
+  }
+</style>
---
+++ 19_单文件组件/Student.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="demo">
+    <h2>{{name}}</h2>
+    <button @click='out'>shuchu</button>
+  </div>
+        
+    
+</template>
+<script>
+export default {
+      name:"Student",
+      data() {
+        return { name: 'xzyd' }
+      },
+      methods: {
+        out(){
+          console.log(this.name);
+        }
+      },
+}
+
+</script>
